83712cac834940c521559e95e55c1ba13ee747f6
Nic-vwalt <36562088+Nic-vwalt@users.noreply.github.com>
2021-02-27 21:50:41 +0200

    login layout

--- workconstruction/lib/palette.dart
+++ workconstruction/lib/palette.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-const TextStyle kHeading = TextStyle(fontSize: 60, fontWeight: FontWeight.bold, color: Colors.black);
+const TextStyle kHeading = TextStyle(
+  fontSize: 60, fontWeight: FontWeight.bold, color: Colors.black54);
+const TextStyle kLabel = TextStyle(
+  fontSize: 25, fontWeight: FontWeight.normal, color: Colors.black26);
+  const TextStyle kButton = TextStyle(
+  fontSize: 30, fontWeight: FontWeight.normal, color: Colors.yellow);
--- workconstruction/lib/screens/welcome/login.dart
+++ workconstruction/lib/screens/welcome/login.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:workconstruction/palette.dart';
 import 'package:workconstruction/screens/widgets/widgets.dart';
+import 'package:country_code_picker/country_code_picker.dart';
+import 'package:country_code_picker/country_localizations.dart';
+import 'package:flutter/material.dart';
 
 class login extends StatelessWidget {
   @override
@@ -9,18 +12,67 @@ class login extends StatelessWidget {
       children: [
         BackgroundImage(),
         Scaffold(
-
+          bottomNavigationBar: BottomAppBar(),
           backgroundColor: Colors.transparent,
           body: Column(
             children: [
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
+              Expanded(
+                child: Align(
+                  alignment: FractionalOffset.bottomCenter,
+                  child: Center(
+                      child: Padding(
+                    padding: const EdgeInsets.all(25.0),
+                    child: Text(
+                      'Welcome',
+                      style: kHeading,
+                    ),
+                  )),
+                ),
+              ),
+              Expanded(
+                  child: Align(
+                alignment: FractionalOffset.bottomCenter,
+                child: Container(
+                  alignment: Alignment.bottomCenter,
                   child: Text(
-                    'Login',
-                    style: kHeading,
+                    'Phonenumber:',
+                    style: kLabel,
                   ),
-                )
+                ),
+              )),
+              Expanded(
+                child: Align(
+                  alignment: FractionalOffset.topCenter,
+                  child: Container(
+                    color: Colors.black45,
+                    height: 50,
+                    width: 200,
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(15.15),
+                    child: Row(
+                      children: [
+                        SizedBox(),
+                        TextField(),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.all(0.0),
+                    child: RaisedButton(
+                      color: Colors.black54,
+                      child: Text(
+                        'Login',
+                        style: kButton,
+                      ),
+                      onPressed: () => key,
+                    ),
+                  ),
+                ),
               )
             ],
           ),
@@ -29,5 +81,3 @@ class login extends StatelessWidget {
     );
   }
 }
-
-
